076f6a4aed95c0a8e4cb7ddd30863095c5034c38
mingogo <mingogo.teng@gmail.com>
2014-10-07 21:26:09 -0400

    add clean.sh

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -r ./.settings
+echo [delte .settings]
+rm -r ./.project
+echo [delte .project]
+rm -r ./.classpath
+echo [delte .classpath]
+rm -r ./target
+echo [delte ./target]
+echo [done.]
