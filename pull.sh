ab843018883e8c0d8d5770ec652a5485a5a064f5
mteng <mingogo.teng@gmail.com>
2014-10-13 13:47:29 -0400

    add push and pull

---
+++ pull.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git status
+git add -A
+read -p "[GIT] Enter commiting message (Add things.):" MESSAGE
+MESSAGE=${MESSAGE:-Add things.}
+git commit -m "$MESSAGE"
+git fetch
+clear
+git branch -a
+read -p "[GIT] Which branch you want to pull from remote?" BRANCH
+echo "[GIT] Pulling $BRANCH from Github."
+git pull origin $BRANCH
+echo "[GIT] Done."
---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git status
+git add -A
+read -p "[GIT] Enter commiting message (Add things.):" MESSAGE
+MESSAGE=${MESSAGE:-Add things.}
+git commit -m "$MESSAGE"
+git fetch
+clear
+git branch -a
+echo "[GIT] Which branch do you want to push to remote?"
+read BRANCH
+echo "[GIT] Pushing $BRANCH to Github."
+git push origin $BRANCH
+echo "[GIT] Done."
