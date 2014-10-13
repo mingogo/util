ac4dad0b8e586fb65f850bf7db3e3bbfb14aff37
mingogo <mingogo.teng@gmail.com>
2014-10-13 14:59:20 -0400

    updated two scripts

--- pull.sh
+++ pull.sh
@@ -5,7 +5,6 @@ read -p "[GIT] Enter commiting message (Add things.):" MESSAGE
 MESSAGE=${MESSAGE:-Add things.}
 git commit -m "$MESSAGE"
 git fetch -p
-clear
 git branch -a
 read -p "[GIT] Which branch you want to pull from remote?" BRANCH
 echo "[GIT] Pulling $BRANCH from Github."
